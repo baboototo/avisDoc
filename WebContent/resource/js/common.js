73cebb88a229d6adf642a850298647dfbb024f75
baboototo <baboototo@gmail.com>
2015-02-10 13:40:00 +0900

    AJ렌터카 개발 가이드

--- WebContent/resource/js/common.js
+++ WebContent/resource/js/common.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function(){
 	createMenuHtml();
+	createFileDownLoadDropdownHtml();
 });
 
 /**
@@ -42,6 +43,74 @@ function createMenuHtml(){
 }
 
 /**
+ * AJ렌터카 개발 가이드 참고문서 HTML 생성
+ */
+function createFileDownLoadDropdownHtml(){
+	var html= '<a class="dropdown-toggle" data-toggle="dropdown" href="#">'
+			+ '		<i class="fa fa-user fa-download"></i>  <i class="fa fa-caret-down"></i>'
+			+ '</a>'
+			+ '<ul class="dropdown-menu dropdown-messages">'
+			+ '		<li>'
+			+ '			<a href="../download/IBSheetHTML.zip">'
+			+ '				<div>'
+			+ '					<strong><i class="fa fa-file-zip-o"></i> IBSheetHTML 개발자가이드 </strong>'
+			+ '				</div>'
+			+ '				<div>'
+			+ '					IBSheet 개발자 가이드 문서 (PDF,CHM)'
+			+ '				</div>'
+			+ '			</a>'
+			+ '		</li>'
+			+ '		<li class="divider"></li>'
+			+ '		<li>'
+			+ '			<a href="../download/IBChartHTML.zip">'
+			+ '				<div>'
+			+ '					<strong><i class="fa fa-file-zip-o"></i> IBChartHTML 개발자가이드 </strong>'
+			+ '				</div>'
+			+ '				<div>'
+			+ '					IBChart 개발자 가이드 문서 (PDF,CHM)'
+			+ '				</div>'
+			+ '			</a>'
+			+ '		</li>'
+			+ '		<li class="divider"></li>'
+			+ '		<li>'
+			+ '			<a href="../download/IBMDITabHTML.zip">'
+			+ '				<div>'
+			+ '					<strong><i class="fa fa-file-zip-o"></i> IBMDITabHTML 개발자가이드 </strong>'
+			+ '				</div>'
+			+ '				<div>'
+			+ '					IBMDITab 개발자 가이드 문서 (PDF,CHM)'
+			+ '				</div>'
+			+ '			</a>'
+			+ '		</li>'
+			+ '		</li>'
+			+ '		<li class="divider"></li>'
+			+ '		<li>'
+			+ '			<a href="../download/AJ_Templates.zip">'
+			+ '				<div>'
+			+ '					<strong><i class="fa fa-file-zip-o"></i> Eclipse HTML/JSP Templates </strong>'
+			+ '				</div>'
+			+ '				<div>'
+			+ '					화면 HTML구성 및 특정 HTML 태그를 사용할 수 있도록 설정된 Eclipse 템플릿 파일 '
+			+ '				</div>'
+			+ '			</a>'
+			+ '		</li>'
+			+ '		<li class="divider"></li>'
+			+ '		<li>'
+			+ '			<a href="../download/com.avis.converter_1.0.0.20140902.jar">'
+			+ '				<div>'
+			+ '					<strong><i class="fa fa-file-zip-o"></i> Eclipse Plug-In </strong>'
+			+ '				</div>'
+			+ '				<div>'
+			+ '					AS-IS 또는 TO-BE JSP파일의 특정소스을 로그로 추출하는 Eclipse 플러그인'
+			+ '				</div>'
+			+ '			</a>'
+			+ '		</li>'
+			+ '</ul">';
+	
+	$("#fileDownLoadDropdown").html(html);
+}
+
+/**
  * AJ렌터카 개발 가이드 메뉴데이터
  * @returns {Array}
  */
@@ -79,7 +148,7 @@ function createMenuJsonData(){
 	       	        	          , {subTitle: "TrSet", 		subUrl: "apiTrSet.html"}
 	       	        	          , {subTitle: "IBSheet", 		subUrl: "apiIBSheet.html"}
 	       	        	          , {subTitle: "IBChart", 		subUrl: "apiIChart.html"}
-	       	        	          , {subTitle: "IBTab", 		subUrl: "apiIBTab.html"}
+	       	        	          , {subTitle: "IBMDITab", 		subUrl: "apiIBMDITab.html"}
 	       	        	          ]
 	       	        }
 	       	        , {
